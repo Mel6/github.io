66df5be9182175611f2577269b1fcfc8be312296
Mel6 <MelVelezE@gmail.com>
2016-03-06 22:22:45 -0500

    Added files via upload

---
+++ MoveMe.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMe : MonoBehaviour {
+
+	public Vector3 speed = new Vector3 (0f,0f,0f);
+	public GameObject [] moveables;
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log (GetComponent<Transform> ().position);
+
+		GetComponent<Transform> ().position += speed;
+		transform.Translate (speed);
+
+
+		foreach (GameObject Sphere in moveables) {
+			Sphere.transform.Translate (speed);
+		}
+	}
+}
